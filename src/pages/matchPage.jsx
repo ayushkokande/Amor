a6726f8612bcd509ece4c83adb06f37291a1db17
Neejor <neejorchakma@gmail.com>
2021-06-01 01:59:24 +0530

    c4

--- src/pages/matchPage.jsx
+++ src/pages/matchPage.jsx
@@ -18,8 +18,8 @@ export default function MatchPage() {
   const [getdata, setGetData] = useState(1);
 
   useEffect(() => {
-    axios.post("http://localhost:4000/giveID", { id: uid }).then(() => {
-      axios.get("http://localhost:4000/getGroup").then((res) => {
+    axios.post("http://amor007.herokuapp.com/giveID", { id: uid }).then(() => {
+      axios.get("http://amor007.herokuapp.com/getGroup").then((res) => {
         store.dispatch({ type: "groupMatch", group: res.data.done });
         console.log(res.data.done);
         if (res.data.done.length === 0) {
